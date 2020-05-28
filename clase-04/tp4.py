9d078357d703b4c58d81fb6ff874b4398582d54d
Pablo Toledo <pabloatm980@gmail.com>
2020-05-27 23:50:24 -0300

    Lint tp4

--- clase-04/tp4.py
+++ clase-04/tp4.py
@@ -1,8 +1,7 @@
 import numpy as np
 import imageio
-import matplotlib.pyplot as plt
 
-# Computar magnitud y fase
+
 def ft_mag_phase(im):
     """Compute magnitude and phase from an image
 
@@ -11,23 +10,24 @@ def ft_mag_phase(im):
 
     Returns:
         Magnitude and phase of `im`
-    """
-    
+        """
+
     # Obtain transform
     transform = np.fft.fft2(im)
-    
+
     # shift to center
     fshift = np.fft.fftshift(transform)
-    
+
     # Obtain magnitud
     magnitude = np.log(np.abs(fshift))
 
     # Calculate phase
-    phase = angle(fshift)
+    phase = np.angle(fshift)
 
     return magnitude, phase
 
-def ift_mag_phase(mag,phase):
+
+def ift_mag_phase(mag, phase):
     """Reconstruct image from magnitude and phase
 
     Arguments:
@@ -36,33 +36,55 @@ def ift_mag_phase(mag,phase):
 
     Returns:
         Reconstructed image from `magnitude` and `phase`
+        """
 
-    """
+    # Reconstruct the spectrum from the magnitud and phase
+    # The exp is used to conterreact the log used before
+    recons_transform = np.exp(mag) * (np.cos(phase) + 1j * np.sin(phase))
 
-    # Reconstrucción del espectro a partir
-    # de magnitud y fase
-    recons_transform = np.exp(magnitude) * (np.cos(phase) + 1j * np.sin(phase))
+    im_rec = np.fft.ifft2(np.fft.ifftshift(recons_transform))\
+        .astype(np.float64)
 
-    # Aplicar inversa a la transformada
-    # obtenida
-    im_rec = np.fft.ifft2(np.fft.ifftshift(recons)).astype(np.float64)
-
-    # Interpolar a valores que acepte una imagen
-    # de 0 a 255
-    img = np.interp(im_rec, (im_rec.min(), im_rec.max()), (0, 255)).astype(np.uint8)
+    # Interpolate values to obtain a real image with values
+    # between 0 and 255
+    img = np.interp(im_rec, (im_rec.min(), im_rec.max()), (0, 255))\
+        .astype(np.uint8)
 
     return img
 
-# Guardar Transformada de Fourier dado 
+
+# Guardar Transformada de Fourier dado
 # Magnitud, fase y filename
 def ftwrite(mag, phase, fn):
+    """Save a Fourier Transform, given a magnitude and a phase
+
+    Arguments:
+        mag (numpy.array): magnitude
+        phase (numpy.array): phase
+        fn (str): Path to save the transform to
+
+    Returns
+    None
+    """
+    # Acaso la magnitud y la fase requieren ser guardadas como un pkl
     pass
+
 
 # Leer Transformada de Fourier
 def ftread(fn):
+    """Read Fourier Transform from a filename
+
+    Arguments:
+        fn (str): Path to the FT image
+
+    Returns:
+        Magnitude and phase of the Fourier Transform
+        """
     pass
+
 
 # Remuestreo (resmaple) en espacio fourier
 # ftresample(im, new_shape).shape == new_shape
-def ftresample(im,new_shape)
+def ftresample(im, new_shape):
+    # Qué es el resampling
     pass
