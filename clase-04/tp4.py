8336e17d11eaeb44481d9a23077c5c1f843ad816
Pablo Toledo <pabloatm980@gmail.com>
2020-05-31 19:44:33 -0300

    Intento tp4
    Enviar y ver qué te dicen

--- clase-04/tp4.py
+++ clase-04/tp4.py
@@ -1,5 +1,12 @@
 import numpy as np
 import imageio
+import pickle
+
+
+FN = 'imageio:camera.png'
+NAME = 'camera'
+MAG_FILE_NAME = f'{NAME}_mag.bmp'
+PHASE_FILE_NAME = f'{NAME}_phase.pkl'
 
 
 def ft_mag_phase(im):
@@ -40,22 +47,20 @@ def ift_mag_phase(mag, phase):
 
     # Reconstruct the spectrum from the magnitud and phase
     # The exp is used to conterreact the log used before
-    recons_transform = np.exp(mag) * (np.cos(phase) + 1j * np.sin(phase))
-
-    im_rec = np.fft.ifft2(np.fft.ifftshift(recons_transform))\
-        .astype(np.float64)
+    recons_shift = np.exp(mag) * (np.cos(phase) + (1j * np.sin(phase)))
+    im_rec = np.real(np.fft.ifft2(np.fft.ifftshift(recons_shift)))
 
     # Interpolate values to obtain a real image with values
     # between 0 and 255
-    img = np.interp(im_rec, (im_rec.min(), im_rec.max()), (0, 255))\
+    im = np.interp(im_rec, (im_rec.min(), im_rec.max()), (0, 255))\
         .astype(np.uint8)
 
-    return img
+    return im
 
 
 # Guardar Transformada de Fourier dado
 # Magnitud, fase y filename
-def ftwrite(mag, phase, fn):
+def ftwrite(mag, phase, name):
     """Save a Fourier Transform, given a magnitude and a phase
 
     Arguments:
@@ -64,23 +69,40 @@ def ftwrite(mag, phase, fn):
         fn (str): Path to save the transform to
 
     Returns
-    None
+        The file names for the save magnitude and phase
     """
-    # Acaso la magnitud y la fase requieren ser guardadas como un pkl
-    pass
+
+    mag_to_write = np.abs(mag)/np.max(mag)
+
+    # Save magnitude
+    imageio.imwrite(MAG_FILE_NAME, mag_to_write)
+
+    # Save phase
+    pickle.dump(phase, open(PHASE_FILE_NAME, 'wb'))
+
+    return MAG_FILE_NAME, PHASE_FILE_NAME
 
 
 # Leer Transformada de Fourier
-def ftread(fn):
-    """Read Fourier Transform from a filename
+def ftread(name):
+    """Read Fourier Transform from a filename.
+    Given a name `name` the program will try to read the files:
+        - '{name}_mag.png'
+        - '{name}_phase.png'
 
     Arguments:
-        fn (str): Path to the FT image
+        name (str): Path to the FT image
 
     Returns:
         Magnitude and phase of the Fourier Transform
         """
-    pass
+
+    phase = pickle.load(open(PHASE_FILE_NAME, 'rb'))
+
+    # Return value in [0,1]
+    mag = imageio.imread(MAG_FILE_NAME)/255
+
+    return mag, phase
 
 
 # Remuestreo (resmaple) en espacio fourier
@@ -88,3 +110,21 @@ def ftread(fn):
 def ftresample(im, new_shape):
     # Qué es el resampling
     pass
+
+
+def main():
+    im = imageio.imread(FN)
+
+    mag, phase = ft_mag_phase(im)
+
+    ftwrite(mag, phase, NAME)
+
+    rmag, rphase = ftread(NAME)
+
+    rim = ift_mag_phase(rmag, rphase)
+
+    imageio.imwrite('m_image.png', rim)
+
+
+if __name__ == '__main__':
+    main()
